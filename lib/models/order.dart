50c5cb43eb449bb142f4c19f3325d829a917ad84
adicomdotir <adicom.ir@gmail.com>
2021-12-06 21:22:31 +0330

    Add models files

---
+++ lib/models/order.dart
@@ -0,0 +1,17 @@
+
+import 'package:first_flutter/models/food.dart';
+import 'package:first_flutter/models/restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order({
+    required this.date,
+    required this.restaurant,
+    required this.food,
+    required this.quantity,
+  });
+}
