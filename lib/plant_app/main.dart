1da895e41e39dcce1a93a456500ac0a6e2a8d405
adicomdotir <adicom.ir@gmail.com>
2021-12-27 00:52:31 +0330

    WP: plant app section 1

---
+++ lib/plant_app/main.dart
@@ -0,0 +1,26 @@
+import 'package:first_flutter/plant_app/constants.dart';
+import 'package:first_flutter/plant_app/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Plant App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/plant_app/screens/home/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:first_flutter/plant_app/screens/home/components/body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset('assets/icons/menu.svg'),
+        onPressed: () {},
+      ),
+    );
+  }
+}
