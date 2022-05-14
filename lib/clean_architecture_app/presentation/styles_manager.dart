c0f0cf7390c375ab89f2c1b4565664da3a5dfe5c
adicomdotir <adicom.ir@gmail.com>
2022-05-14 19:38:08 +0430

    Add style manager

---
+++ lib/clean_architecture_app/presentation/styles_manager.dart
@@ -0,0 +1,42 @@
+import 'package:first_flutter/clean_architecture_app/presentation/font_manager.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTextStyle(
+    double fontSize, String fontFamaly, FontWeight fontWeight, Color color) {
+  return TextStyle(
+      fontSize: fontSize,
+      fontFamily: fontFamaly,
+      color: color,
+      fontWeight: fontWeight);
+}
+
+TextStyle getLightStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.light, color);
+}
+
+TextStyle getReqularStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.reqular, color);
+}
+
+TextStyle getMediumStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.medium, color);
+}
+
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.semiBold, color);
+}
+
+TextStyle getBoldStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.bold, color);
+}
+
