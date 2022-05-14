544f4885db8a93c8970d82d6cbd5c57c35ab7efd
adicomdotir <adicom.ir@gmail.com>
2022-05-14 19:11:23 +0430

    Add color & font manager

---
+++ lib/clean_architecture_app/presentation/color_manager.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class ColorManager {
+  static Color primary = HexColor.fromHex('#ED9728');
+  static Color darkGrey = HexColor.fromHex('#525252');
+  static Color grey = HexColor.fromHex('#737477');
+  static Color lighGrey = HexColor.fromHex('#9E9E9E');
+  static Color primaryOpacity70 = HexColor.fromHex('#BCED9728');
+}
+
+extension HexColor on Color {
+  static Color fromHex(String hexColorString) {
+    hexColorString = hexColorString.replaceAll('#', '');
+    if (hexColorString.length == 6) {
+      hexColorString = 'FF' + hexColorString;
+
+    }
+    return Color(int.parse(hexColorString, radix: 16));
+  }
+}
