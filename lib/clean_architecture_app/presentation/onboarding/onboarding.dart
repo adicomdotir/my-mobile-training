181889bb6c6a122bdbc8d31012a4727b4f007140
adicomdotir <adicom.ir@gmail.com>
2022-05-16 00:02:16 +0430

    Add onboarding base page

--- lib/clean_architecture_app/presentation/onboarding/onboarding.dart
+++ lib/clean_architecture_app/presentation/onboarding/onboarding.dart
@@ -1,5 +1,9 @@
+import 'package:first_flutter/clean_architecture_app/presentation/resources/assets_manager.dart';
 import 'package:first_flutter/clean_architecture_app/presentation/resources/color_manager.dart';
+import 'package:first_flutter/clean_architecture_app/presentation/resources/strings_manager.dart';
+import 'package:first_flutter/clean_architecture_app/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -9,11 +13,42 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  PageController _pageController = PageController();
+
+  List<SliderObject> _getSliderData() => [
+    SliderObject(AppStrings.onBoardingTitle1, AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
+    SliderObject(AppStrings.onBoardingTitle2, AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
+    SliderObject(AppStrings.onBoardingTitle3, AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
+    SliderObject(AppStrings.onBoardingTitle4, AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: Center(child: Text('Welcome to on boarding')),
+      appBar: AppBar(
+        elevation: AppSize.s1_5,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark
+        ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        itemBuilder: (ctx, index) {
+          
+        }),
     );
   }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
 }
