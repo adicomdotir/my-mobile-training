6cce1617f4fee90c47d3dd4d2ab17aeab239daa7
adicomdotir <adicom.ir@gmail.com>
2022-05-15 21:59:00 +0430

    Add route to app

--- lib/clean_architecture_app/presentation/splash/splash.dart
+++ lib/clean_architecture_app/presentation/splash/splash.dart
@@ -1,7 +1,9 @@
+import 'package:first_flutter/clean_architecture_app/presentation/resources/assets_manager.dart';
+import 'package:first_flutter/clean_architecture_app/presentation/resources/color_manager.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
-  const SplashView({ Key? key }) : super(key: key);
+  const SplashView({Key? key}) : super(key: key);
 
   @override
   State<SplashView> createState() => _SplashViewState();
@@ -10,8 +12,13 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+      body: Center(
+        child: Image(
+          image: AssetImage(ImageAssets.splashLogo),
+        ),
+      ),
     );
   }
 }
