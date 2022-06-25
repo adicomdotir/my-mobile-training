266b59cffcb72c193bf2a51e87cff758fc2cb0c9
adicomdotir <adicom.ir@gmail.com>
2022-06-25 23:19:39 +0430

    Add circle chart for category

--- lib/budget_app/screens/category_screen.dart
+++ lib/budget_app/screens/category_screen.dart
@@ -1,4 +1,7 @@
+import 'package:first_flutter/budget_app/helpers/color_helper.dart';
 import 'package:first_flutter/budget_app/models/category_model.dart';
+import 'package:first_flutter/budget_app/models/expense_model.dart';
+import 'package:first_flutter/budget_app/widgets/radial_painter.dart';
 import 'package:first_flutter/food_delivery_app/models/restaurant.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +17,13 @@ class CategoryScreen extends StatefulWidget {
 class CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach((Expense expense) {
+      totalAmountSpent += expense.cost;
+    });
+    final double amountLeft = widget.category.maxAmount - totalAmountSpent;
+    final double percent = amountLeft / widget.category.maxAmount;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category.name),
@@ -26,25 +36,38 @@ class CategoryScreenState extends State<CategoryScreen> {
         ],
       ),
       body: SingleChildScrollView(
-        child: Column(children: [
-          Container(
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(20),
-            height: 250,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(0, 2),
-                  blurRadius: 6
-                )
-              ]
-            ),
-          )
-        ],),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(20),
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0, 2),
+                        blurRadius: 6)
+                  ]),
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                    bgColor: Colors.grey,
+                    lineColor: getColor(context, percent),
+                    percent: percent,
+                    width: 15),
+                child: Center(
+                  child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
