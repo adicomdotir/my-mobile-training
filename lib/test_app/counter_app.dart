278bf34e81e8e0f5f2559a30aad477e65f18e93b
adicomdotir <adicom.ir@gmail.com>
2022-08-28 22:43:43 +0430

    Add future & stream builder for training

--- lib/test_app/counter_app.dart
+++ lib/test_app/counter_app.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Counter {
@@ -20,7 +23,19 @@ class CounterApp extends StatefulWidget {
 }
 
 class _CounterAppState extends State<CounterApp> {
+  late Future<int> future;
+  StreamController<int> streamController = StreamController();
   int counter = 0;
+
+  @override
+  void initState() {
+    future = getIntNumber();
+    getStreamInt().listen((event) {
+      print(event);
+      streamController.add(event);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,19 +45,44 @@ class _CounterAppState extends State<CounterApp> {
           title: Text('Counter App'),
         ),
         body: Center(
-            child: Column(
-          children: [
-            ElevatedButton(onPressed: () {
-              setState(() {
-                counter++;
-              });
-            }, child: Text('Click')),
-            Text(
-              counter.toString(),
-              style: TextStyle(fontSize: 28, color: Colors.black),
-            ),
-          ],
-        )),
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter++;
+                    });
+                  },
+                  child: Text('Click')),
+              Text(
+                counter.toString(),
+                style: TextStyle(fontSize: 28, color: Colors.black),
+              ),
+              FutureBuilder<int>(
+                future: future,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final int num = snapshot.data!;
+                    return Text(num.toString());
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
+              StreamBuilder<int>(
+                stream: streamController.stream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final int num = snapshot.data!;
+                    return Text(num.toString());
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              )
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             setState(() {
@@ -53,5 +93,17 @@ class _CounterAppState extends State<CounterApp> {
         ),
       ),
     );
+  }
+
+  Future<int> getIntNumber() async {
+    await Future.delayed(Duration(milliseconds: 2000));
+    return Random().nextInt(100);
+  }
+
+  Stream<int> getStreamInt() async* {
+    for (var i = 0; i < 10; i++) {
+      await Future.delayed(Duration(milliseconds: 1000));
+      yield Random().nextInt(100);
+    }
   }
 }
