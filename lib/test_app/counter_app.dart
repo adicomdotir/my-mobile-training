55b21f403d28eea2154030d328d0ddfe413c93ce
adicomdotir <adicom.ir@gmail.com>
2022-09-12 00:28:23 +0430

    Add user model with generator

--- lib/test_app/counter_app.dart
+++ lib/test_app/counter_app.dart
@@ -1,7 +1,70 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+
+class User {
+  final int id;
+  final String username;
+  final bool active;
+
+  User(
+    this.id,
+    this.username,
+    this.active,
+  );
+
+  User copyWith({
+    int? id,
+    String? username,
+    bool? active,
+  }) {
+    return User(
+      id ?? this.id,
+      username ?? this.username,
+      active ?? this.active,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'username': username,
+      'active': active,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      map['id'] as int,
+      map['username'] as String,
+      map['active'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'User(id: $id, username: $username, active: $active)';
+
+  @override
+  bool operator ==(covariant User other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.username == username &&
+        other.active == active;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ username.hashCode ^ active.hashCode;
+}
 
 class Counter {
   int value = 0;
@@ -105,5 +168,34 @@ class _CounterAppState extends State<CounterApp> {
       await Future.delayed(Duration(milliseconds: 1000));
       yield Random().nextInt(100);
     }
+  }
+}
+
+class AnimController extends StatefulWidget {
+  const AnimController({Key? key}) : super(key: key);
+
+  @override
+  State<AnimController> createState() => _AnimControllerState();
+}
+
+class _AnimControllerState extends State<AnimController>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
